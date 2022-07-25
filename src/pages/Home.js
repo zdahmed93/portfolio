1370b5de5552a5e0b06bc87af655489f117410ab
Ahmed Zribi <ahmedzribiahmed@gmail.com>
2022-07-25 14:57:45 +0100

    Add content to the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import webImage from '../images/photo-2.png'
 
 function Home() {
   return (
-    <div className="alert alert-info mt-5" role="alert">
-      Hello my name is Ahmed. I am a Fullstack Web Developer.
+    <div className="alert alert-info mt-5 container" role="alert">
+      <img src="/images/photo-1.png" className='with-border-radius' width="150px" alt="My Photo" />
+      <p>Hello my name is Ahmed. I am a Fullstack Web Developer.</p>
+      <img src={webImage} style={{width: '100%'}} alt="Web" />
     </div>
   )
 }
